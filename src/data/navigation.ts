b0e5a5eebc444fd2269c105384b2b4ec1448d29e
Craig Puxty <craig_puxty@talktalk.net>
2023-10-13 13:57:55 +0100

    Added navigation data.

---
+++ src/data/navigation.ts
@@ -0,0 +1,7 @@
+export const navigation = [
+  { name: 'Home', link: '/' },
+  { name: 'About', link: '/#about' },
+  { name: 'Skills', link: '/#skills' },
+  { name: 'Projects', link: '/#projects' },
+  { name: 'Contact', link: '/#contact' },
+];

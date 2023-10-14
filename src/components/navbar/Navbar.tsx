7709b6a4cafa5d48f804965626208b18f032967f
Craig Puxty <craig_puxty@talktalk.net>
2023-10-14 13:12:57 +0100

    Updated Navbar with animation.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,9 @@
 // React router
 import { Link } from 'react-router-dom';
 
+// Hooks
+import useAnimation from '../../hooks/useAnimation';
+
 // Components
 import Navigation from '../navigation/Navigation';
 import MobileIcon from './mobile_icon/MobileIcon';
@@ -9,8 +12,14 @@ import MobileIcon from './mobile_icon/MobileIcon';
 import { navigation } from '../../data/navigation';
 
 export default function Navbar() {
+  useAnimation('.navbar-animate', '#navbar', {
+    y: -300,
+    duration: 1,
+    immediateRender: false,
+  });
+
   return (
-    <header className='navbar'>
+    <header className='navbar navbar-animate' id='navbar'>
       <div className='container'>
         <Link to='/' className='logo'>
           Craig Puxty

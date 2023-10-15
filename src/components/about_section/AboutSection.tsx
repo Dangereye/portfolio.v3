386a0d94f0941540a0d411998a42a1d26f64e539
Craig Puxty <craig_puxty@talktalk.net>
2023-10-15 11:47:32 +0100

    Added AboutSection & about-section.scss. Updated global.scss.

---
+++ src/components/about_section/AboutSection.tsx
@@ -0,0 +1,82 @@
+// Components
+import { Link } from 'react-router-dom';
+// import ArticleHeading from '../../components/ArticleHeading';
+// import Button from '../../components/Button';
+
+// Animation hook
+import useAnimation from '../../hooks/useAnimation';
+// import useModal from '../../hooks/useModal';
+
+export default function AboutSection() {
+  //   const { closeModal, downloadCV } = useModal();
+  // Animation
+  // useAnimation(element id,trigger id,{animation options},{trigger options})
+  useAnimation('.about-animate', '#about', {
+    duration: 1,
+    scale: 1.3,
+    immediateRender: false,
+  });
+
+  useAnimation('.about-animate-text', '#about', {
+    y: -300,
+    delay: 0.4,
+    stagger: 0.05,
+    immediateRender: false,
+    ease: 'back.out(1.4)',
+  });
+
+  return (
+    <section className='section section-about' id='about'>
+      <div className='container'>
+        <div className='image about-animate'>
+          <div className='image__wrapper'>
+            {/* <Image
+              src='/img/about/profile.webp'
+              width={500}
+              height={500}
+              layout='responsive'
+              alt='Craig Puxty'
+            /> */}
+          </div>
+        </div>
+        <div className='text'>
+          <header className='article__header about-animate-text'>
+            {/* <ArticleHeading lead='About me' h2='Front-end developer' /> */}
+          </header>
+          <p className='body-text body-text--lead about-animate-text'>
+            Specialising in React.js and Next.js development.
+          </p>
+          <p className='body-text about-animate-text'>
+            Hi! I'm Craig Puxty, a bespoke front-end developer from a small town
+            in Flintshire, North Wales, UK.
+          </p>
+          <p className='body-text about-animate-text'>
+            Over several years, I've dedicated much of my free time to gaining
+            proficiency in several front-end technologies and design tools,
+            together with some back-end knowledge.
+          </p>
+          <p className='body-text about-animate-text'>
+            My development process is meticulous, following fundamental design
+            principles: "keep it simple, stupid" (KISS), "don't repeat yourself"
+            (DRY), simple navigation, visual hierarchy, consistency, etc.
+          </p>
+          <p className='body-text about-animate-text'>
+            My mission is to create beautiful, modern software with custom,
+            high-quality imagery - that's accessible, scalable, responsive and
+            performance-driven.
+          </p>
+          <div className='btns about-animate-text'>
+            <Link to='/#contact'>
+              <a className='btn btn--large btn--primary '>Contact me</a>
+            </Link>
+            {/* <Button
+              name='Download CV'
+              classes='btn--large btn--tertiary'
+              func={downloadCV}
+            /> */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,10 +1,12 @@
 // Components
 import HeroSection from '../hero_section/HeroSection';
+import AboutSection from '../about_section/AboutSection';
 
 export default function Main() {
   return (
     <main className='main'>
       <HeroSection />
+      <AboutSection />
     </main>
   );
 }

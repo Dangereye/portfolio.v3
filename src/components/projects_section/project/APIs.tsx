eed61dc24a85c16186d1364633120661608fc7a9
Craig Puxty <craig_puxty@talktalk.net>
2023-10-20 13:22:57 +0100

    Updated APIs.

--- src/components/projects_section/project/APIs.tsx
+++ src/components/projects_section/project/APIs.tsx
@@ -5,26 +5,26 @@ type APIsProps = {
 };
 
 export default function Apis({ name, apis, anim }: APIsProps) {
-  return (
-    <>
-      {apis && (
-        <div className={`apis ${anim}`}>
-          <h4 className='heading heading--h3'>APIs</h4>
-          <div className='btns tags'>
-            {apis.map((item, i) => (
-              <a
-                key={`${name}-api-${i}`}
-                href={item.anchor}
-                target='_blank'
-                rel='noreferrer'
-                className='btn btn--tag'
-              >
-                {item.name}
-              </a>
-            ))}
-          </div>
+  if (apis && apis.length > 0) {
+    return (
+      <div className={`apis ${anim}`}>
+        <h4 className='heading heading--h3'>APIs</h4>
+        <div className='btns tags'>
+          {apis.map((item, i) => (
+            <a
+              key={`${name}-api-${i}`}
+              href={item.anchor}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='btn btn--tag'
+            >
+              {item.name}
+            </a>
+          ))}
         </div>
-      )}
-    </>
-  );
+      </div>
+    );
+  }
+
+  return null;
 }

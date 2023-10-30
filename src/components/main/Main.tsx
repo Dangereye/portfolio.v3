a0d63175472af676120afda4e2fd67d68cf15514
Craig Puxty <craig_puxty@talktalk.net>
2023-10-30 14:31:01 +0000

    Added ContactSection & contact-section.scss. Updated Main.

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -3,6 +3,7 @@ import HeroSection from '../hero_section/HeroSection';
 import AboutSection from '../about_section/AboutSection';
 import SkillsSection from '../skills_section/SkillsSection';
 import ProjectsSection from '../projects_section/ProjectsSection';
+import ContactSection from '../contact_section/ContactSection';
 
 export default function Main() {
   return (
@@ -11,6 +12,7 @@ export default function Main() {
       <AboutSection />
       <SkillsSection />
       <ProjectsSection />
+      <ContactSection />
     </main>
   );
 }

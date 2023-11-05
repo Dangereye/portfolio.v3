887cbfc7cfcc72fee4aa2a51f22f43406ea9bb99
Craig Puxty <craig_puxty@talktalk.net>
2023-11-05 11:25:32 +0000

    Updated SkillsSection & skills-section.scss.

--- src/components/skills_section/SkillsSection.tsx
+++ src/components/skills_section/SkillsSection.tsx
@@ -5,16 +5,15 @@ import { Link } from 'react-router-dom';
 import useAnimation from '../../hooks/useAnimation';
 
 // Components
-// import ArticleHeading from '../../components/ArticleHeading';
-// import ListGroup from '../../components/ListGroup';
-// import Anchor from '../../components/Anchor';
+import HeadingSection from '../text_elements/HeadingSection';
+import BodyText from '../text_elements/BodyText';
+import ListGroup from '../list_group/ListGroup';
 
 // Data
 import { environment } from '../../data/skills/enviroment';
 import { designTools } from '../../data/skills/designTools';
 import { frontEnd } from '../../data/skills/frontEnd';
 import { backEnd } from '../../data/skills/backEnd';
-import ListGroup from '../list_group/ListGroup';
 
 export default function SkillsSection() {
   // useAnimation(element id,trigger id,{from options},{to options})
@@ -25,7 +24,7 @@ export default function SkillsSection() {
       scale: 1.3,
       opacity: 0,
     },
-    { scale: 1, opacity: 1, duration: 1, delay: 0.5, ease: 'power2.Out' }
+    { scale: 1, opacity: 1, duration: 1, ease: 'power2.Out' }
   );
 
   useAnimation(
@@ -66,43 +65,56 @@ export default function SkillsSection() {
     <section className='section skills-section' id='skills'>
       <div className='container skills-animate-bg'>
         <div className='background '></div>
-        <header className='section__header'>
-          <div className='heading heading--lead skills-animate-text'>
-            Skills
-          </div>
-          <h2 className='heading heading--h2 skills-animate-text'>
-            Knowledge & Experience
-          </h2>
-
-          <p className='body-text skills-animate-text'>
-            Experienced in building static websites,{' '}
-            <Link
-              to='https://reactjs.org'
-              rel='noopener noreferrer'
-              target='_blank'
-            >
-              React.js
-            </Link>{' '}
-            and{' '}
-            <Link
-              to='https://nextjs.org/'
-              rel='noopener noreferrer'
-              target='_blank'
-            >
-              Next.js
-            </Link>{' '}
-            applications, and customisation of eCommerce platforms such as{' '}
-            <Link to='https://business.adobe.com/uk/products/magento/magento-commerce.html'>
-              Magento
-            </Link>{' '}
-            and{' '}
-            <Link to='https://wordpress.com/website-builder'>WordPress</Link>.
-            Listed are typical environmental setup and design tools, frequently
-            used front-end technologies, frameworks and libraries. I also have
-            some experience with back-end technologies - DB connection, routing,
-            schemas etc.
-          </p>
-        </header>
+        <div className='typography'>
+          <HeadingSection
+            lead='skills'
+            heading='knowledge & experience'
+            anim='skills-animate-text'
+          />
+          <BodyText
+            text={
+              <>
+                Experienced in building static websites,{' '}
+                <Link
+                  to='https://reactjs.org'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                >
+                  React.js
+                </Link>{' '}
+                and{' '}
+                <Link
+                  to='https://nextjs.org/'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                >
+                  Next.js
+                </Link>{' '}
+                applications, and customisation of eCommerce platforms such as{' '}
+                <Link
+                  to='https://business.adobe.com/uk/products/magento/magento-commerce.html'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                >
+                  Magento
+                </Link>{' '}
+                and{' '}
+                <Link
+                  to='https://wordpress.com/website-builder'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                >
+                  WordPress
+                </Link>
+                . Listed are typical environmental setup and design tools,
+                frequently used front-end technologies, frameworks and
+                libraries. I also have some experience with back-end
+                technologies - DB connection, routing, schemas etc.
+              </>
+            }
+            anim='skills-animate-text'
+          />
+        </div>
         <div className='lists'>
           <ListGroup
             heading={environment.heading}

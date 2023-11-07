02790992a5811173dd6086cf14dbfc16ab38f058
Craig Puxty <craig_puxty@talktalk.net>
2023-11-07 15:56:43 +0000

    Updated buttons.scss.

--- src/data/sourceCode.ts
+++ src/data/sourceCode.ts
@@ -1,15 +1,15 @@
 export const sourceCode = [
   {
+    name: 'portfolio.v3',
+    anchor: 'https://github.com/Dangereye/portfolio.v3',
+  },
+  {
     name: 'movies',
     anchor: 'https://github.com/Dangereye/movies-ts',
   },
   {
     name: 'star wars',
     anchor: 'https://github.com/Dangereye/star-wars-ts',
-  },
-  {
-    name: 'portfolio.v3',
-    anchor: 'https://github.com/Dangereye/portfolio.v3',
   },
   {
     name: 'games',

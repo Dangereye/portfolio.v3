eafeab3c365fbf0838e08c5fd0f7cd162e1f6bfb
Craig Puxty <craig_puxty@talktalk.net>
2023-11-14 13:24:26 +0000

    Updated Logo & logo.svg.

--- src/svgs/Logo.tsx
+++ src/svgs/Logo.tsx
@@ -1,36 +1,28 @@
 export default function Logo() {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 315 316'>
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 321 321'>
+      <path
+        fill='none'
+        stroke='#189fd4'
+        strokeLinecap='round'
+        strokeMiterlimit='10'
+        strokeWidth='25'
+        d='M125 302.97c0-19.61 15.89-35.5 35.5-35.5s35.5 15.89 35.5 35.5M160.5 210.2V267v-56.8Z'
+      />
+      <path
+        fill='none'
+        stroke='#189fd4'
+        strokeMiterlimit='10'
+        strokeWidth='25'
+        d='M170.5 91.87h122c5.52 0 10 4.48 10 10v98.33c0 5.52-4.48 10-10 10h-132V101.87c0-5.52 4.48-10 10-10Z'
+      />
       <path
         fill='none'
         stroke='#fff'
         strokeLinecap='round'
         strokeMiterlimit='10'
-        strokeWidth='15'
-        d='M127.5 210.5h-110c-5.52 0-10-4.48-10-10v-183c0-5.52 4.48-10 10-10h280c5.52 0 10 4.48 10 10v38'
-      />
-      <path
-        fill='none'
-        stroke='#189fd4'
-        strokeLinecap='round'
-        strokeMiterlimit='10'
         strokeWidth='25'
-        d='M157.5 210.5v60-60Z'
-      />
-      <path
-        fill='none'
-        stroke='#189fd4'
-        strokeMiterlimit='10'
-        strokeWidth='25'
-        d='M167.5 85.5h130c5.52 0 10 4.48 10 10v105c0 5.52-4.48 10-10 10h-140v-115c0-5.52 4.48-10 10-10Z'
-      />
-      <path
-        fill='none'
-        stroke='#189fd4'
-        strokeLinecap='round'
-        strokeMiterlimit='10'
-        strokeWidth='25'
-        d='M120 308.5c0-20.71 16.79-37.5 37.5-37.5s37.5 16.79 37.5 37.5'
+        d='M115.1 210.2H27.97c-5.23 0-9.47-4.24-9.47-9.47V27.49c0-5.23 4.24-9.47 9.47-9.47h265.07c5.23 0 9.47 4.24 9.47 9.47v18.97'
       />
     </svg>
   );

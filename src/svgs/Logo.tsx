a4be454eab10ebc5e52baed40b8036e7d1425921
Craig Puxty <craig_puxty@talktalk.net>
2023-11-14 12:52:36 +0000

    Added Logo & logo.svg.

---
+++ src/svgs/Logo.tsx
@@ -0,0 +1,29 @@
+export default function Logo() {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 315 466'>
+      <path
+        fill='none'
+        stroke='#fff'
+        strokeLinecap='round'
+        strokeMiterlimit='10'
+        strokeWidth='15'
+        d='M137.5 308.5h-120c-5.52 0-10-4.48-10-10v-281c0-5.52 4.48-10 10-10h280c5.52 0 10 4.48 10 10v121'
+      />
+      <path
+        fill='none'
+        stroke='#189fd4'
+        strokeLinecap='round'
+        strokeMiterlimit='10'
+        strokeWidth='15'
+        d='M82.5 458.5c0-41.42 33.58-75 75-75s75 33.58 75 75M157.5 308.5v75-75Z'
+      />
+      <path
+        fill='none'
+        stroke='#189fd4'
+        strokeMiterlimit='10'
+        strokeWidth='15'
+        d='M167.5 158.5h130c5.52 0 10 4.48 10 10v130c0 5.52-4.48 10-10 10h-140v-140c0-5.52 4.48-10 10-10Z'
+      />
+    </svg>
+  );
+}
